f4e10f13e6264a8cdd88738841bad9a0f3e19dc4
Luiz Victor <luiz.victor100@hotmail.com>
2020-02-24 20:35:04 -0300

    montando arquivos estaticos

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const app = express();
+const bodyParset = require('body-parser');
 
 
 //view engine
 app.set('view engine', 'ejs');
 
+
+//arquivo estratico
+app.use(express.static('public'));
+
+//tratar formularios bod-parser
+app.use(bodyParset.urlencoded({extended: false}));
+app.use(bodyParset.json());
 
 app.get('/', (req, resp) => {
     resp.render("index");
