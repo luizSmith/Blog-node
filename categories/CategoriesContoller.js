818dd57c363bd5c46552fa5bbd065d541abffe4c
Luiz Victor <luiz.victor100@hotmail.com>
2020-02-25 18:27:32 -0300

    implementando slugify

--- categories/CategoriesContoller.js
+++ categories/CategoriesContoller.js
@@ -1,9 +1,30 @@
 const express = require("express");
 const router = express.Router();
 
+const Category = require('./Category');
+const slugify = require("slugify");
+
 //objeto utilizado para criar as rotas
 router.get('/admin/categories/new', (req, resp) => {
     resp.render("admin/categories/new");
+});
+
+router.post("/categories/save", (req,resp) => {
+    var title = req.body.title;
+    if (title != undefined) {
+
+        Category.create({
+            title: title,
+            // npm install --save slugify
+            //"Desenvolvimento web" => "desenvolvimento-web"
+            slug: slugify(title)
+        }).then(() => {
+            resp.redirect('/');
+        })
+
+    } else {
+        resp.redirect("/admin/categories/new");
+    }
 });
 
 module.exports = router;
