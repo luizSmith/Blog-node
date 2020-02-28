9531b40d2ab228cea032dd5b49665b87cbbf963b
Luiz Victor <luiz.victor100@hotmail.com>
2020-02-28 00:01:35 -0300

    Criando artigo

--- articles/ArticlesContoller.js
+++ articles/ArticlesContoller.js
@@ -20,4 +20,34 @@ router.get('/admin/articles', (req, resp) => {
     });
 });
 
+router.get('/admin/articles/new', (req,resp) => {
+    Category.findAll({
+        raw:true
+    }).then(categories => {
+        resp.render('admin/articles/new',{
+            categories:categories
+        });
+    })
+});
+
+
+router.post('/articles/save',(req,resp) => {
+    var title = req.body.title;
+    var category = req.body.category;
+    var body = req.body.body;
+
+    if (title != undefined && isNaN(title)) {
+        Article.create({
+            title: title,
+            slug: slugify(title),
+            body: body,
+            tbCategoryId: category
+        }).then(() => {
+            resp.redirect('/admin/articles');
+        })
+    } else {
+        resp.redirect('/admin/articles');
+    }
+});
+
 module.exports = router;
