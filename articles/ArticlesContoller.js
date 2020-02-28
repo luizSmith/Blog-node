e2ac0c6939038db9c2c8d5904cec28aa69a7d51f
Luiz Victor <luiz.victor100@hotmail.com>
2020-02-27 23:22:34 -0300

    iniciando CRUD artigos

--- articles/ArticlesContoller.js
+++ articles/ArticlesContoller.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const router = express.Router();
 
+const Category = require("../categories/Category");
+const Article = require("./Article");
+const slugify = require("slugify");
+
 //objeto utilizado para criar as rotas
-router.get('/articles', (req, resp) => {
-    resp.send("ROTA ARTICLES");
+router.get('/admin/articles', (req, resp) => {
+    Article.findAll({
+        include: [{model: Category}],
+        raw:true,
+        order: [
+            ['id','desc']
+        ]
+    }).then(articles => {
+        resp.render('admin/articles/index',{
+            articles:articles
+        });
+    });
 });
 
 module.exports = router;
