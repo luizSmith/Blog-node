4adfee7587c794cfea4e03f4bd0f96d4a73318dd
Luiz Victor <luiz.victor100@hotmail.com>
2020-04-09 04:25:39 -0300

    Crud usuario completo

---
+++ public/js/main.js
@@ -0,0 +1,4 @@
+function alertaErro (msg) {
+    $("#alerta").html(msg);
+    $("#alerta").show();
+}
--- users/UsersController.js
+++ users/UsersController.js
@@ -4,7 +4,14 @@ const User = require("./User");
 const bcrypt = require('bcryptjs');
 
 router.get('/admin/users', (req, resp) => {
-    resp.send("Listagem de usuarios");
+    User.findAll().then(users => {
+        resp.render("admin/users/index",{
+            users:users
+        });
+    }).catch(erro => {
+        resp.redirect("/admin/users/create");
+        console.log(erro)
+    });
 });
 
 router.get('/admin/users/create', (req, resp) => {
@@ -40,9 +47,63 @@ router.post('/user/create',(req,resp) => {
             });
         }
     })
-
-    
 });
 
+router.get("/admin/users/edit/:id", (req,resp) => {
+    var id = req.params.id;
+
+    if (id != undefined && !isNaN(id)) {
+        User.findByPk(id).then(user=>{
+            resp.render("admin/users/edit",{
+                user:user
+            })
+        }).catch(errp=> {
+            resp.redirect("/admin/users");
+        })
+    } else {
+        resp.redirect("/admin/users");
+    }
+});
+
+router.post('/users/update', (req, resp) => {
+    var id = req.body.id;
+    var email = req.body.email;
+    var password = req.body.password;
+
+    var salt = bcrypt.genSaltSync(10);
+    var hash = bcrypt.hashSync(password,salt);
+
+
+    if (id != undefined && !isNaN(id)) {
+        User.update({
+            email: email,
+            password: hash
+        },{
+            where: {
+                id: id
+            }
+        }).then(() => {
+            resp.redirect("/admin/users");
+        })
+    } else {
+        resp.redirect("/admin/users");
+    }
+});
+
+router.post('/users/delete', (req,resp) => {
+    var id = req.body.id;
+
+    if (id != undefined && !isNaN(id)) { 
+            User.destroy({
+                where:{
+                    id: id
+                }
+            }).then(() => {
+                resp.redirect("/admin/users");
+            });
+    } else { 
+        resp.redirect("/admin/users");
+    }
+});
 
 module.exports = router;
